8835ea1ab15816c66010bc21e40795f44581b11e
Rhoj <88019904+RhojAnn@users.noreply.github.com>
2026-01-21 13:14:54 +1100

    Cleaned up directory

--- api_build.bat
+++ api_build.bat
@@ -32,13 +32,8 @@ if %ERRORLEVEL% EQU 0 (
 
 g++ -shared -o bin\asi_api.dll asi\asi_api.cpp -lcomdlg32 ^
     -Iasi ^
-    -IOpenCV-MinGW-Build-OpenCV-4.5.5-x64/include ^
     -Lasi ^
-    -LOpenCV-MinGW-Build-OpenCV-4.5.5-x64/x64/mingw/lib ^
-    -lASICamera2 ^
-    -lopencv_core455 -lopencv_highgui455 -lopencv_imgproc455 -lopencv_imgcodecs455
-
-copy /Y lib\asi\*.dll bin\
+    -lASICamera2
 
 if %ERRORLEVEL% EQU 0 (
     echo Build successful: bin\asi_api.dll
